84304626a89ad431b0379c17cb62992d4475fd91
junyao <canjunyao@gmail.com>
2023-02-10 23:19:58 +0000

    change2

--- lab6part1/Main.java
+++ lab6part1/Main.java
@@ -3,7 +3,7 @@ package lab6part1;
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal("Seth",22);
-        Wolf wolf = new Wolf("uncle",100);
+        Wolf wolf = new Wolf("Uncle",100);
         wolf.makeNoise();
         Parrot parrot = new Parrot("Donuts",12);
         parrot.makeNoise();
